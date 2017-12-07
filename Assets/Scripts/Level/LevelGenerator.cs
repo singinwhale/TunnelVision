51a4c0cd9988d3823f64b00c2c5a34e163894dd4
singinwhale <singinwhale@protonmail.com>
2017-12-07 15:20:41 +0100

    Added Math.Net DLLs and Test Scene

---
+++ Assets/Scripts/Level/LevelGenerator.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using MathNet.Numerics.Interpolation;
+
+public class LevelGenerator : MonoBehaviour
+{
+    private CubicSpline splineXY;
+    private CubicSpline splineXZ;
+
+    [Range(-10,10)]
+    public double t;
+    // Use this for initialization
+    void Start()
+    {
+        double[] x = {1, 3, 0};
+        double[] y = {0, 0, -3};
+        double[] z = {0, 3, 0};
+        splineXY = CubicSpline.InterpolateNatural(x, y);
+        splineXZ = CubicSpline.InterpolateNatural(x, z);
+        
+
+        splineXZ.Interpolate(1.5);
+        splineXY.Interpolate(1.5);
+    }
+	
+	// Update is called once per frame
+	void Update ()
+	{
+	    Vector3 res;
+	    res.x = (float)t;
+	    res.y = (float)splineXZ.Interpolate(t);
+	    res.z = (float)splineXZ.Interpolate(t);
+		Debug.DrawLine(Vector3.zero, res);
+	}
+}
